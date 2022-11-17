dbc349902ef2cf34d93d3db15655bceecb2a347e
Zin Lin Phyo <zinlinphyo.mm@gmail.com>
2022-11-17 11:27:21 +0630

    Initial Commit

---
+++ Flixpedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixpedia
+//
+//  Created by Zin Lin Phyo on 17/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
